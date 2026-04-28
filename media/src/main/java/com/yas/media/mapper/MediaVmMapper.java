bd0f577a4b454a24c15c4de99e9a90d5c19b0c51
= <ngduong5124@gmail.com>
2026-04-29 00:15:13 +0700

    ci: media, adjust MediaVmMapper and BaseKafka (due to kafka outdated)

--- media/src/main/java/com/yas/media/mapper/MediaVmMapper.java
+++ media/src/main/java/com/yas/media/mapper/MediaVmMapper.java
@@ -5,6 +5,21 @@ import com.yas.media.model.Media;
 import com.yas.media.viewmodel.MediaVm;
 import org.mapstruct.Mapper;
 
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
 @Mapper(componentModel = "spring")
 public interface MediaVmMapper extends BaseMapper<Media, MediaVm> {
+
+    @Override
+    @Mapping(target = "filePath", ignore = true)
+    Media toModel(MediaVm vm);
+
+    @Override
+    @Mapping(target = "url", ignore = true)
+    MediaVm toVm(Media m);
+
+    @Override
+    @Mapping(target = "filePath", ignore = true)
+    void partialUpdate(@MappingTarget Media m, MediaVm v);
 }
