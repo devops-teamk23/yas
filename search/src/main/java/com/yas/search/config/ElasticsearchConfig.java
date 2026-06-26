7456ee2cd3402fbbb7393ed9ef4af5a8ff9c76f5
LeThanhCong <ltchcmus@gmail.com>
2026-06-26 19:45:00 +0700

    Fix ElasticsearchConfig HttpHeaders import

--- search/src/main/java/com/yas/search/config/ElasticsearchConfig.java
+++ search/src/main/java/com/yas/search/config/ElasticsearchConfig.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
-import org.springframework.http.HttpHeaders;
+import org.springframework.data.elasticsearch.support.HttpHeaders;
 
 @Configuration
 public class ElasticsearchConfig extends ElasticsearchConfiguration {
